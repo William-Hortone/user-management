273b1bc85f7c4e2e6eb42617214388b4bc667b6d
William Hortone <willhortone@gmail.com>
2023-10-10 13:02:12 +0800

    done with the login authentification

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,54 +1,88 @@
 import React, { useState } from "react";
 import styles from "../auth.module.scss";
 import loginImg from "../../../assets/login.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import Card from "../../../components/card/Card";
+import { auth } from "../../../firebase/config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
+import { Loader } from "../../../components";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    console.log(email, password);
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setIsLoading(false);
+        toast.success("Login successful");
+        navigate("/");
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        toast.error(error.message);
+        setIsLoading(false);
+      });
+  };
 
   return (
-    <section className={`container ${styles.auth}`}>
-      <div className={styles.img}>
-        <img src={loginImg} alt="Login image" width="400" />
-      </div>
-      <Card>
-        <div className={styles.form}>
-          <h2>Login</h2>
+    <>
+      {isLoading && <Loader />}
 
-          <form>
-            <input
-              type="text"
-              placeholder="Email"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button className="--btn --btn-primary --btn-block">Login</button>
-            <div className={styles.links}>
-              <Link to="/reset">Reset Password</Link>
-            </div>
-            <p>-- or --</p>
-          </form>
-          <button className="--btn --btn-danger --btn-block">
-            <FaGoogle color="#fff" /> Login with Google
-          </button>
-          <span className={styles.register}>
-            <p>Don't have an account? </p>
-            <Link to="/register"> Register</Link>
-          </span>
+      <section className={`container ${styles.auth}`}>
+        <div className={styles.img}>
+          <img src={loginImg} alt="Login image" width="400" />
         </div>
-      </Card>
-    </section>
+        <Card>
+          <div className={styles.form}>
+            <h2>Login</h2>
+
+            <form onSubmit={handleLogin}>
+              <input
+                type="text"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit" className="--btn --btn-primary --btn-block">
+                Login
+              </button>
+              <div className={styles.links}>
+                <Link to="/reset">Reset Password</Link>
+              </div>
+              <p>-- or --</p>
+            </form>
+            <button className="--btn --btn-danger --btn-block">
+              <FaGoogle color="#fff" /> Login with Google
+            </button>
+            <span className={styles.register}>
+              <p>Don't have an account? </p>
+              <Link to="/register"> Register</Link>
+            </span>
+          </div>
+        </Card>
+      </section>
+    </>
   );
 };
 
