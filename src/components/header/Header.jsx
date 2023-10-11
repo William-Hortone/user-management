92951a60d27bf1c0f9586ba079e1ddf764250585
William Hortone <willhortone@gmail.com>
2023-10-11 14:43:39 +0800

    added the state management with redux

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,6 +6,8 @@ import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../../firebase/config";
 import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { SET_ACTIVE_USER } from "../../redux/slice/authSlice";
 
 const logo = (
   <div className={styles.logo}>
@@ -32,13 +34,23 @@ const Header = () => {
   const [userName, setUserName] = useState("");
   const navigate = useNavigate();
 
+  const dispatch = useDispatch();
+
   // Monitor current sign in user
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         const uid = user.uid;
-        console.log(user.displayName);
+        // console.log(user);
         setUserName(user.displayName);
+
+        dispatch(
+          SET_ACTIVE_USER({
+            email: user.email,
+            userName: user.displayName,
+            userID: user.uid,
+          })
+        );
       } else {
       }
     });
