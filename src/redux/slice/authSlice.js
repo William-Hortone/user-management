a5dfd7e8e8799091a5179bdbcd07b6ac36b0094d
William Hortone <willhortone@gmail.com>
2023-10-13 00:10:54 +0800

    display the menu links according to the user

--- src/redux/slice/authSlice.js
+++ src/redux/slice/authSlice.js
@@ -30,6 +30,7 @@ const authSlice = createSlice({
 });
 
 export const { SET_ACTIVE_USER, REMOVE_ACTIVE_USER } = authSlice.actions;
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 export const selectEmail = (state) => state.auth.email;
 export const selectUserName = (state) => state.auth.userName;
 export const selectUserID = (state) => state.auth.userID;
