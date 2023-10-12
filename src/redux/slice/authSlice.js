b6ec66a13540eefbdc713dd0f1f9d53662c8e390
William Hortone <willhortone@gmail.com>
2023-10-12 14:03:53 +0800

    removed the active user in redux

--- src/redux/slice/authSlice.js
+++ src/redux/slice/authSlice.js
@@ -12,17 +12,24 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     SET_ACTIVE_USER: (state, action) => {
-      console.log(action.payload);
+      // console.log(action.payload);
       const { email, userName, userID } = action.payload;
       state.isLoggedIn = true;
       state.email = email;
       state.userName = userName;
       state.userID = userID;
     },
+    REMOVE_ACTIVE_USER: (state, action) => {
+      state.isLoggedIn = false;
+      state.email = null;
+      state.userName = null;
+      state.userID = null;
+      console.log(state.isLoggedIn);
+    },
   },
 });
 
-export const { SET_ACTIVE_USER } = authSlice.actions;
+export const { SET_ACTIVE_USER, REMOVE_ACTIVE_USER } = authSlice.actions;
 export const selectEmail = (state) => state.auth.email;
 export const selectUserName = (state) => state.auth.userName;
 export const selectUserID = (state) => state.auth.userID;
